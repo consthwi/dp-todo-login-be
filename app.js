298739e39b674e5a577fb830273da44268f6e8a2
Hwiwon Kang <syaron9364@gmail.com>
2024-10-21 17:19:14 +0900

    upload to heroku

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose
     console.log("db connection failed", err);
   });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
